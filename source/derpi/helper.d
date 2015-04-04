ac54efb139df35ad7a53df9a5a7be0069eb3c3e4
Mihail-K <Mihail.K@fimu.tk>
2015-04-04 13:07:54 -0400

    Added parse table.

---
+++ source/derpi/helper.d
@@ -0,0 +1,8 @@
+
+module derpi.helper;
+
+alias Rule = int;
+
+alias Terminal = int;
+
+alias NonTerminal = int;
--- source/derpi/table.d
+++ source/derpi/table.d
@@ -1,4 +1,48 @@
 
 module derpi.table;
 
+import derpi.helper;
 
+class ParseTable
+{
+
+	private
+	{
+
+		Rule[Terminal][NonTerminal] table;
+
+		int[][Rule] rhs;
+	
+	}
+
+	int[] opIndex(Rule rule)
+	{
+		return rhs[rule];
+	}
+
+	Rule opIndex(NonTerminal n, Terminal t)
+	{
+		return table[n][t];
+	}
+
+	void opIndexAssign(int[] rhs, Rule rule)
+	in
+	{
+		assert(rule !in this.rhs);
+	}
+	body
+	{
+		this.rhs[rule] = rhs;
+	}
+
+	void opIndexAssign(Rule rule, NonTerminal n, Terminal t)
+	in
+	{
+		assert(n !in table || t !in table[n]);
+	}
+	body
+	{
+		table[n][t] = rule;
+	}
+
+}
